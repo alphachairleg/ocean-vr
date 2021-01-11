9f62b3ccf557c58fc195dc954c8f58c4a3f71179
morris <morrisxlee@gmail.com>
2021-01-11 23:34:07 +1000

    Fish imported + Fish movement AI
    Fish imported + Fish movement AI

---
+++ Liminal VR calming game/Assets/scripts/Flock.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{   //Fish start speed
+    public float speed = 1f;
+
+    //Fishes roation speed
+    float rotationSpeed = 5.0f;
+
+    float minSpeed = 0.8f;
+    float maxSpeed = 2.0f;
+
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighbourDistance = 3.0f;
+
+    public Vector3 newGoalPos;
+
+    bool turning = false;
+
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+
+        //Fishes start with with random speed between these numbers
+        //speed = Random.Range(1f, 3);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!turning)
+        {
+            newGoalPos = this.transform.position - other.gameObject.transform.position;
+        }
+        turning = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        turning = false;
+    }
+
+
+    void Update()
+    {
+        if(turning)
+        {
+            Vector3 direction = newGoalPos - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    rotationSpeed * Time.deltaTime);
+
+            speed = Random.Range(minSpeed, maxSpeed);
+        }
+        else
+        {
+            if (Random.Range(0, 10) < 1)
+                ApplyRules();
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+
+        /*if (Vector3.Distance(transform.position, Vector3.zero) >= GlobalFlock.tankSize)
+        {
+            turning = true;
+        }
+        else
+            turning = false;
+        if (turning)
+        {
+            Vector3 direction = Vector3.zero - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    rotationSpeed * Time.deltaTime);
+            speed = Random.Range(0.5f, 1);
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+                ApplyRules();
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);*/
+
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = GlobalFlock.allFish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = GlobalFlock.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach (GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if(dist <= neighbourDistance )
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(dist < 2.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+        if(groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        rotationSpeed * Time.deltaTime);
+                                
+        }
+
+    }
+
+}
---
+++ Liminal VR calming game/Assets/scripts/GlobalFlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    //public List<GameObject> Fishes;
+    public static int tankSize = 15;
+    public GameObject goalPrefab;
+
+    //Fishes spawn number
+    static int numFish = 45;
+    public static GameObject[] allFish = new GameObject[numFish];
+
+    public static Vector3 goalPos = Vector3.zero;
+
+    void Start()
+    {
+        for(int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize));
+            allFish[i] = (GameObject)Instantiate(fishPrefab,pos, Quaternion.identity);
+        }
+    }
+
+
+    void Update()
+    {
+        if(Random.Range(0, 10000) < 50)
+        goalPos = new Vector3(Random.Range(-tankSize, tankSize),
+                               Random.Range(-tankSize, tankSize),
+                               Random.Range(-tankSize, tankSize));
+
+        goalPrefab.transform.position = goalPos;
+    }
+}
