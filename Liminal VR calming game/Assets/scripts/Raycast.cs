24bd0981ddc403caa88cac345f7aad48b585a64c
morris <morrisxlee@gmail.com>
2021-01-16 20:38:38 +1000

    Post processing bug fix, added hand control, bubble machanic
    Post processing bug fix, added hand control, bubble machanic

---
+++ Liminal VR calming game/Assets/scripts/Raycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public class ControllerInput : MonoBehaviour
+    {
+        public Transform pointerTransform; void Update()
+
+        {
+            if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+            {
+                RaycastDirection();
+            }
+            if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+            {
+                RaycastDirection();
+            }
+        }
+
+        private void RaycastDirection()
+        {
+            RaycastHit selection;
+            if (Physics.Raycast(pointerTransform.position, pointerTransform.forward, out selection))
+            {
+                if (selection.collider.gameObject.CompareTag("Bubble"))
+                {
+                    //spawn particle effect
+                    Destroy(selection.collider.gameObject);
+                }
+            }
+        }
+    }
+}
--- Liminal VR calming game/Assets/scripts/physics.cs
+++ Liminal VR calming game/Assets/scripts/physics.cs
@@ -6,27 +6,62 @@ public class physics : MonoBehaviour
 {
     public float thrust = .00000000040f;
     public Rigidbody rb;
-  //  public float Shakespeed;
+    private bool bubbleleft;
+    private float Timer;
+
+
+    //  public float Shakespeed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
+        bubbleleft = true;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(thrust == 1) { 
-        rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        Timer++;
+        if (thrust == 1)
+        {
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }
+
+        if (bubbleleft == true)
+        {
+            transform.position += transform.forward * Time.deltaTime;
+
+            if (Timer >= 80)
+            {
+                bubbleleft = false;
+                TimerReset();
             }
-       /* Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * Shakespeed);
-        newPos.y = transform.position.y;
-        newPos.z = transform.position.z;
-        transform.position = newPos;
-       accidental tornado code
-       */
+        }
+
+        if (bubbleleft == false)
+        {
+            transform.position -= transform.forward * Time.deltaTime;
+            if (Timer >= 80)
+            {
+                bubbleleft = true;
+                TimerReset();
+            }
+        }
+
+        /* Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * Shakespeed);
+         newPos.y = transform.position.y;
+         newPos.z = transform.position.z;
+         transform.position = newPos;
+        accidental tornado code
+        */
     }
-  
+    public void TimerReset()
+    {
+        Timer = 0;
+    }
+
 }
+
